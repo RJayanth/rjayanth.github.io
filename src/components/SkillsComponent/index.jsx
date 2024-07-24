bf6556450809291414d6d9d29a7e65f5deaeeb81
Jayanth R <Jayanth.R2@ibm.com>
2024-07-24 17:24:23 +0530

    feat: Skills tiles dark mode support

--- src/components/SkillsComponent/index.jsx
+++ src/components/SkillsComponent/index.jsx
@@ -1,61 +1,64 @@
-import useThemeChange from "../../hooks/useThemeChange";
+import useThemeChange from '../../hooks/useThemeChange';
 import htmlIcon from '../../assets/images/htmlIcon.svg';
+import htmlIconDark from '../../assets/images/htmlIconDark.svg';
 import cssIcon from '../../assets/images/cssIcon.svg';
+import cssIconDark from '../../assets/images/cssIconDark.svg';
 import jsIcon from '../../assets/images/jsIcon.svg';
 import reactIcon from '../../assets/images/reactIcon.svg';
 import reduxIcon from '../../assets/images/reduxIcon.svg';
 import debugIcon from '../../assets/images/debugIcon.svg';
 
 const SkillsComponent = () => {
-    const { isDarkTheme } = useThemeChange();
-    return (
-        <div className="skills-container">
-        <div className="skills-header"> KEY SKILLS </div>
-        <div className="skills-icon-container">
-          <div>
-            <img
-              src={isDarkTheme ? htmlIcon : htmlIcon}
-              alt="logo"
-              className="skills-icon"
-            />
-          </div>          <div>
-            <img
-              src={isDarkTheme ? cssIcon : cssIcon}
-              alt="logo"
-              className="skills-icon"
-            />
-          </div>
-          <div>
-            <img
-              src={isDarkTheme ? jsIcon : jsIcon}
-              alt="logo"
-              className="skills-icon"
-            />
-          </div>
-          <div>
-            <img
-              src={isDarkTheme ? reactIcon : reactIcon}
-              alt="logo"
-              className="skills-icon"
-            />
-          </div>
-          <div>
-            <img
-              src={isDarkTheme ? reduxIcon : reduxIcon}
-              alt="logo"
-              className="skills-icon"
-            />
-          </div>
-          <div title='Debugging'>
-            <img
-              src={isDarkTheme ? debugIcon : debugIcon}
-              alt="logo"
-              className="skills-icon"
-            />
-          </div>
+  const { isDarkTheme } = useThemeChange();
+  return (
+    <div className="skills-container">
+      <div className="skills-header"> KEY SKILLS </div>
+      <div className="skills-icon-container">
+        <div>
+          <img
+            src={isDarkTheme ? htmlIconDark : htmlIcon}
+            alt="logo"
+            className={`skills-icon ${isDarkTheme ? 'skills-icon-dark' : 'skills-icon-light' }`}
+          />
+        </div>
+        <div>
+          <img
+            src={isDarkTheme ? cssIconDark : cssIcon}
+            alt="logo"
+            className={`skills-icon ${isDarkTheme ? 'skills-icon-dark' : 'skills-icon-light' }`}
+          />
+        </div>
+        <div>
+          <img
+            src={isDarkTheme ? jsIcon : jsIcon}
+            alt="logo"
+            className={`skills-icon ${isDarkTheme ? 'skills-icon-dark' : 'skills-icon-light' }`}
+          />
+        </div>
+        <div>
+          <img
+            src={isDarkTheme ? reactIcon : reactIcon}
+            alt="logo"
+            className={`skills-icon ${isDarkTheme ? 'skills-icon-dark' : 'skills-icon-light' }`}
+          />
+        </div>
+        <div>
+          <img
+            src={isDarkTheme ? reduxIcon : reduxIcon}
+            alt="logo"
+            className={`skills-icon ${isDarkTheme ? 'skills-icon-dark' : 'skills-icon-light' }`}
+          />
+        </div>
+        <div title="Debugging">
+          <img
+            src={isDarkTheme ? debugIcon : debugIcon}
+            alt="logo"
+            className={`skills-icon ${isDarkTheme ? 'skills-icon-dark' : 'skills-icon-light' }`}
+          />
         </div>
       </div>
-    )
-}
+    </div>
+  );
+};
 
 export default SkillsComponent;
