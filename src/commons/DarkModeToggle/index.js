4d98d51a1972f2b861d9cc35efc7881a96841ac0
Jayanth R <Jayanth.R2@ibm.com>
2024-07-24 20:18:38 +0530

    feat: use MUI Toggle for changing dark & light modes"

--- src/commons/DarkModeToggle/index.js
+++ src/commons/DarkModeToggle/index.js
@@ -3,17 +3,19 @@ import modeToggleDark from '../../assets/images/modeToggleDark.svg';
 import { useThemeContext } from '../../themes';
 
 import './index.css';
+import ModeToggle from '../ModeToggle';
 
 const DarkModeToggle = () => {
 
   const { toggleTheme, isDarkTheme } = useThemeContext();
   return (
     <div className="mode-toggle-container" onClick={toggleTheme}>
-      <img
+      {/* <img
         src={isDarkTheme ? modeToggleDark : modeToggleLight}
         alt="Turn on / off Dark mode"
         className="mode-toggle-icon"
-      />
+      /> */}
+      <ModeToggle />
     </div>
   );
 };
