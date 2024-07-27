48b269681a620fb05c063f1d2e21914431bbd994
Jayanth R <Jayanth.R2@ibm.com>
2024-07-27 17:18:38 +0530

    feat: Consumed  HOC to accomodate view more functionality in AboutMe component

--- src/components/AboutMeComponent/index.jsx
+++ src/components/AboutMeComponent/index.jsx
@@ -5,9 +5,23 @@ import DisplayPicture from "../DisplayPicture";
 import FileDownload from "../../commons/FileDownload";
 
 import './index.scss';
+import withViewMore from "../withViewMore";
+import useMediaQuery from "../../hooks/useMediaQuery";
 
 const AboutMeComponent = () => {
     const { isDarkTheme } = useThemeContext();
+    const isMobileView = useMediaQuery('(max-width: 767px)');
+    const AdditionalSection = () => {
+        return (
+            <>
+                <p className="am-desc">{ABOUT_ME.EXPERIENCE}</p>
+                <p className="am-desc">{ABOUT_ME.ROLE}</p>
+            </>
+        )
+    }
+
+    const AdditionalSectionMobileView= withViewMore(AdditionalSection);
+
     return (
         <div className={`about-me-container ${isDarkTheme ? 'am-dark' : 'am-light'}`}>
             <DisplayPicture />
@@ -16,10 +30,9 @@ const AboutMeComponent = () => {
             <SectionHeadingText title='about me' customClassName='about-me-heading' />
             <div className="am-description-container">
                 <p className="am-desc">{ABOUT_ME.HEADING}</p>
-                <p className="am-desc">{ABOUT_ME.EXPERIENCE}</p>
-                <p className="am-desc">{ABOUT_ME.ROLE}</p>
+                {isMobileView ? <AdditionalSectionMobileView /> : <AdditionalSection />}
             </div>
-            <FileDownload />
+            <FileDownload title='Resume' fileName='Jayanth_R_Resume' path='Jayanth_R_Resume' />
         </div>
     )
 }
